741e61857286451a79136edfdf8f0464bf12a600
Robert Corty <rcorty@gmail.com>
2017-10-25 14:26:15 -0400

    continued CRANification
    Signed-off-by: Robert Corty <rcorty@gmail.com>

--- R/plot_actogram.R
+++ R/plot_actogram.R
@@ -14,7 +14,9 @@
 #' f <- file.path(system.file(package = 'actogrammr'), 'testdata')
 #' d <- read_clock_lab_files(file_names = list.files(path = f, full.names = TRUE))
 #' b <- bin_data(data = d, minutes_per_bin = 6)
+#' \dontrun{
 #' plot_actogram(data = b, start_date = '2010-01-01')
+#' }
 
 plot_actogram <- function(data,
                           start_date = min(data$date),
